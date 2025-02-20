9420c97ebc5a6dbff5aa3549e03f56d8606c6c4a
Titapon Keawbua <118767390+64160047@users.noreply.github.com>
2025-02-20 22:56:51 +0700

    Herosection

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="w-full h-[60vh] flex items-center bg-gradient-to-r from-purple-900 to-blue-900 text-white">
+      <!-- 🔹 ด้านซ้าย: ข้อความ -->
+      <div class="w-1/2 px-16">
+        <h1 class="text-3xl font-bold text-green-300">
+          Speech and Text Understanding Research Team (STU)
+        </h1>
+        <p class="mt-4 text-gray-300">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis penatibus sapien fusce vel curae elementum.
+        </p>
+      </div>
+  
+      <!-- 🔹 ด้านขวา: STU -->
+      <div class="w-1/2 flex justify-center items-center">
+        <h1 class="text-[120px] font-extrabold text-white">STU</h1>
+      </div>
+    </section>
+  </template>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import NavBar from '../components/NavBar.vue'
+import HeroSection from '../components/HeroSection.vue'
 </script>
 
 <template>
   <main>
- 
     <NavBar />
-    
+    <HeroSection />
   </main>
 </template>
